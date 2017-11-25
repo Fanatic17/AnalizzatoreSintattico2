845cc1d56820663ac1f7c80c45d54a0b43ab1e77
Fanatic17 <fanaticcar_17@hotmail.it>
2017-11-25 17:50:32 +0100

    GUI

--- src/main/scala/GUI/Gui.java
+++ src/main/scala/GUI/Gui.java
@@ -1,13 +1,17 @@
 package GUI;
 
 import javafx.application.Application;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeView;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import main.ParseProgram;
+
+import java.lang.reflect.Array;
 
 public class Gui extends Application {
 
@@ -15,27 +19,52 @@ public class Gui extends Application {
         launch(args);
     }
 
+
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Analizzatore Sintattico");
-        TextField program = new TextField();
-        program.setPromptText("Inserisci il programma");
-        TreeView<String> visual = new TreeView<>();
+        Stage window = primaryStage;
+        window.setTitle("Analizzatore Sintattico");
 
-        HBox layout= new HBox();
-        BorderPane pannello = new BorderPane();
-        pannello.setTop(layout);
-        layout.getChildren().addAll(program,visual);
+        Button button = new Button("OK");
+        Button button2 = new Button("Indietro");
+        Label label = new Label("Inserisci il Programma");
+
+        TextArea textArea = new TextArea();
+        VBox layout3 = new VBox(30);
+        HBox layout2 = new HBox(15);
+        VBox layout = new VBox(10);
+        HBox layout4 = new HBox(40);
+        layout.setPadding(new Insets(20,20,20,20));
+        layout.getChildren().addAll(textArea, button);
+        layout2.setPadding(new Insets(20,20,20,20));
+        layout2.getChildren().addAll(label);
+        layout3.getChildren().addAll(layout2,layout);
+        layout4.getChildren().add(button2);
+        Scene scene = new Scene(layout3,355,250);
+        Scene scene2 = new Scene(layout4,500,300);
+
+        window.setScene(scene);
+        window.show();
+
+        button.setOnAction((ActionEvent event) -> {
+            String result = (ParseProgram.parse(textArea.getText())).toString();
+            window.setScene(scene2);
+        });
+
+        button2.setOnAction((ActionEvent event) -> {
+            window.setScene(scene);
+        });
 
 
-        primaryStage.show();
     }
+
 
 
     public void printGUI(String input){
 
         System.out.print(input);
 
-        launch(input);
+
     }
 }
--- src/main/scala/main/ParseProgram.scala
+++ src/main/scala/main/ParseProgram.scala
@@ -1,20 +1,21 @@
 package main
 
-import javafx.stage.Stage
 
-import GUI.Gui
 import rules.Rules
 
 object ParseProgram extends Rules{
 
-  def main(args:Array[String]): Unit ={
+  def main(args:Array[String]): Unit = {
 
-    val gui = new Gui()
-    println("input:"+args(0))
-    gui.printGUI(parseAll(program,args(0)).toString)
 
-    //Fare un metodo che prende in ingresso  parseAll(program,args(0)) e stampa l'interfaccia grafica
+  }
 
+
+  def parse(input :String): ParseProgram.ParseResult[Any] ={
+    println("input:"+input)
+    println(parseAll(program,input))
+  return parseAll(program,input)
+    //gui.printGUI(parseAll(program,args(0)).toString)
   }
 
 }
